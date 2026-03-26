27f868e93ecb24ff8c655121ad0e82e5f8ff2a98
lua <araujo.leticia2@aluno.ifsp.edu.br>
2026-03-26 20:23:16 -0300

    Add files via upload

---
+++ exercicio 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char** argv)
+
+{
+	 float d, a;
+	
+	
+	cout << "informe a diagonal do quadrado: ";
+	cin >> d;
+	
+	a = (d * d)/2;
+	
+	cout << "a area do quadrado calculada: " << a;
+	
+	return 0;
+	
+	
+}
